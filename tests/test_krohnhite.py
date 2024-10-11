796e59df7094eb17daa2b1a8f1ea68f8e522b666
Aria <aria.hajikhani@levylab.org>
2024-10-11 10:38:38 -0400

    Initial working copy from KH branch

---
+++ tests/test_krohnhite.py
@@ -0,0 +1,20 @@
+#%% Imports
+import sys
+import os
+from config_watcher import get_latest_config
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from levylabinst.KrohnHite import KrohnHite
+
+kh_address = 'tcp://localhost:29160'
+# %% Database handling
+latest_config_data = get_latest_config()
+kh_config_info = latest_config_data.get('kh_config_info', {})
+# TODO: Fix error with the config dictionary
+
+kh = KrohnHite('kh', kh_address, config={'kh_config_info': ""})
+
+#%%
+kh._send_command("HELP")
+# TODO: Make functions for each API returned from the above method.
+# %%
