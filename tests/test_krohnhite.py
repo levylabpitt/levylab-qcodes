9f627ac01bfe92ad96429bb91b08f6a7b27d5e67
Aria <aria.hajikhani@levylab.org>
2024-10-18 13:20:38 -0400

    update

--- tests/test_krohnhite.py
+++ tests/test_krohnhite.py
@@ -21,16 +21,16 @@ kh._send_command("HELP")
 kh.set_all_channels(kh_config_info)
 
 # %%
+kh.get_all_channels()
+
+# %%
 kh.set_channel(2)
 
 # %%
-kh.get_channel(1)
+kh.get_channel(2)
 
 # %%
 print("Sending configuration:", kh_config_info)
-
-# %%
-kh.get_all_channels()
 
 # %%
 print("KH Config Info being sent:", kh_config_info)
